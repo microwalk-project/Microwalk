36876f960ef71e6cad222c41563a14d46c0c6df0
Jan Wichelmann <jan.wichelmann@hotmail.de>
2020-06-25 12:24:41 +0200

    Clean up wrapper code

--- PinTracerWrapper/PinTracerWrapper.cpp
+++ PinTracerWrapper/PinTracerWrapper.cpp
@@ -2,12 +2,13 @@
 This program is a lightweight wrapper for the investigated library.
 In trace mode (using Pin) it periodically reads test input file names from stdin and loads the given testcases.
 Note: If the target library is written in C++ and exports mangled names, this program should also be compiled as C++ so it can find those exports.
+      If the target library is written in C, do not forget to use `extern "C"` when including the headers.
 */
 
 /* INCLUDES */
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 
 // OS-specific imports and helper macros
 #if defined(_WIN32)
@@ -45,7 +46,7 @@ _NOINLINE void InitTarget()
 {
 	/*** TODO INSERT THE TARGET INITIALIZATION CODE HERE ***/
 	BCRYPT_ALG_HANDLE dummy;
-	BCryptOpenAlgorithmProvider(&dummy, BCRYPT_AES_ALGORITHM, NULL, 0);
+	BCryptOpenAlgorithmProvider(&dummy, BCRYPT_AES_ALGORITHM, nullptr, 0);
 	BCryptCloseAlgorithmProvider(dummy, 0);
 }
 
@@ -65,30 +66,30 @@ _NOINLINE void RunTarget(FILE* input)
 
 	BCRYPT_ALG_HANDLE aesAlg;
 	BCRYPT_KEY_HANDLE aesKey;
-	BCryptOpenAlgorithmProvider(&aesAlg, BCRYPT_AES_ALGORITHM, NULL, 0);
+	BCryptOpenAlgorithmProvider(&aesAlg, BCRYPT_AES_ALGORITHM, nullptr, 0);
 	DWORD keyObjectSize;
 	DWORD data;
-	BCryptGetProperty(aesAlg, BCRYPT_OBJECT_LENGTH, (PUCHAR)& keyObjectSize, sizeof(DWORD), &data, 0);
+	BCryptGetProperty(aesAlg, BCRYPT_OBJECT_LENGTH, reinterpret_cast<PUCHAR>(&keyObjectSize), sizeof(DWORD), &data, 0);
 
-	BYTE* keyObj = (BYTE*)malloc(keyObjectSize);
+	BYTE* keyObj = static_cast<BYTE*>(malloc(keyObjectSize));
 	DWORD blockLength;
-	BCryptGetProperty(aesAlg, BCRYPT_BLOCK_LENGTH, (PBYTE)& blockLength, sizeof(DWORD), &data, 0);
-	BCryptSetProperty(aesAlg, BCRYPT_CHAINING_MODE, (PBYTE)BCRYPT_CHAIN_MODE_ECB, sizeof(BCRYPT_CHAIN_MODE_ECB), 0);
-	BCryptGenerateSymmetricKey(aesAlg, &aesKey, keyObj, keyObjectSize, (PBYTE)secret_key, sizeof(secret_key), 0);
+	BCryptGetProperty(aesAlg, BCRYPT_BLOCK_LENGTH, reinterpret_cast<PUCHAR>(&blockLength), sizeof(DWORD), &data, 0);
+	BCryptSetProperty(aesAlg, BCRYPT_CHAINING_MODE, PUCHAR(BCRYPT_CHAIN_MODE_ECB), sizeof(BCRYPT_CHAIN_MODE_ECB), 0);
+	BCryptGenerateSymmetricKey(aesAlg, &aesKey, keyObj, keyObjectSize, static_cast<PUCHAR>(secret_key), sizeof(secret_key), 0);
 
 	DWORD cipherTextSize;
-	BCryptEncrypt(aesKey, plain, sizeof(plain), NULL, NULL, 0, NULL, 0, &cipherTextSize, 0);
-	BYTE* cipherText = (BYTE*)malloc(cipherTextSize);
-	BCryptEncrypt(aesKey, plain, sizeof(plain), NULL, NULL, 0, cipherText, cipherTextSize, &data, 0);
+	BCryptEncrypt(aesKey, plain, sizeof(plain), nullptr, nullptr, 0, nullptr, 0, &cipherTextSize, 0);
+	BYTE* cipherText = static_cast<BYTE*>(malloc(cipherTextSize));
+	BCryptEncrypt(aesKey, plain, sizeof(plain), nullptr, nullptr, 0, cipherText, cipherTextSize, &data, 0);
 }
 
-// Notify PIN that a testcase starts/ends.
+// Pin notification functions.
 // These functions (and their names) must not be optimized away by the compiler, so Pin can find and instrument them.
 // The return values reduce the probability that the compiler uses these function in other places as no-ops (Visual C++ did do this in some experiments).
 #pragma optimize("", off)
 extern "C" _EXPORT _NOINLINE int PinNotifyTestcaseStart(int t) { return t + 42; }
 extern "C" _EXPORT _NOINLINE int PinNotifyTestcaseEnd() { return 42; }
-extern "C" _EXPORT _NOINLINE int PinNotifyStackPointer(uint64_t spMin, uint64_t spMax) { return (int)(spMin + spMax + 42); }
+extern "C" _EXPORT _NOINLINE int PinNotifyStackPointer(uint64_t spMin, uint64_t spMax) { return static_cast<int>(spMin + spMax + 42); }
 #pragma optimize("", on)
 
 // Reads the stack pointer base value and transmits it to Pin.
@@ -107,7 +108,7 @@ void ReadAndSendStackPointer()
 //     The current action is read from stdin.
 //     A line with "t" followed by a numeric ID, and another line with a file path determining a new testcase, that is subsequently loaded and fed into the target function, while calling PinNotifyNextFile() beforehand.
 //     A line with "e 0" terminates the program.
-extern "C" _EXPORT void TraceFunc(int argc, const char** argv)
+extern "C" _EXPORT void TraceFunc()
 {
 	// First transmit stack pointer information
 	ReadAndSendStackPointer();
@@ -129,7 +130,7 @@ extern "C" _EXPORT void TraceFunc(int argc, const char** argv)
         // Exit or process given testcase
         if(command == 'e')
             break;
-        else if(command == 't')
+        if(command == 't')
         {
             // Read testcase file name
             gets_s(inputBuffer, sizeof(inputBuffer));
@@ -137,7 +138,7 @@ extern "C" _EXPORT void TraceFunc(int argc, const char** argv)
             // Load testcase file and run target function
             FILE* inputFile;
             errno_t err = fopen_s(&inputFile, inputBuffer, "rb");
-            if(inputFile == NULL)
+            if(inputFile == nullptr)
             {
                 strerror_s(errBuffer, sizeof(errBuffer), err);
                 fprintf(stderr, "Error opening input file '%s': %s\n", inputBuffer, errBuffer);
@@ -155,6 +156,6 @@ extern "C" _EXPORT void TraceFunc(int argc, const char** argv)
 int main(int argc, const char** argv)
 {
     // Run target function
-    TraceFunc(argc, argv);
+    TraceFunc();
     return 0;
 }
