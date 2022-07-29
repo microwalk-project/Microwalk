b6168a7d473a02b7c61cee61d69c40aa5aaac6a8
Jan Wichelmann <j.wichelmann@uni-luebeck.de>
2022-07-29 18:37:22 +0200

    Bump version

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.4")]
+[assembly: AssemblyFileVersion("3.0.4")]
+[assembly: AssemblyInformationalVersion("3.0.4")]
